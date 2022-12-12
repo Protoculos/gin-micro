23192dcdaf78ad3d91849a4517c1bb217ecf3ca8
Protoculos <mazdak2012@gmail.com>
2022-12-13 01:20:04 +0300

    boilerplate

--- main.go
+++ main.go
@@ -1,31 +1,8 @@
 package main
 
-import (
-	"encoding/xml"
-
-	"github.com/gin-gonic/gin"
-)
-
-// func IndexHandler(c *gin.Context) {
-// 	name := c.Params.ByName("name")
-// 	c.JSON(200, gin.H{
-// 		"message": "Hello " + name,
-// 	})
-// }
+import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
-	router.GET("/", IndexHandler)
 	router.Run(":5000")
-}
-
-type Person struct {
-	XMLName   xml.Name `xml:"person"`
-	FirstName string   `xml:"firstName,attr"`
-	LastName  string   `xml:"lastName,attr"`
-}
-
-func IndexHandler(c *gin.Context) {
-	c.XML(200, Person{FirstName: "Mohamed",
-		LastName: "Labouardy"})
 }
