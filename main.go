01ddb8e68d703b6e81cd5c46b7220f5acc3cd753
Protoculos <mazdak2012@gmail.com>
2022-12-13 01:15:10 +0300

    xml response

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/xml"
+
+	"github.com/gin-gonic/gin"
+)
+
+// func IndexHandler(c *gin.Context) {
+// 	name := c.Params.ByName("name")
+// 	c.JSON(200, gin.H{
+// 		"message": "Hello " + name,
+// 	})
+// }
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "hello world",
-		})
-	})
+	router.GET("/", IndexHandler)
 	router.Run(":5000")
+}
+
+type Person struct {
+	XMLName   xml.Name `xml:"person"`
+	FirstName string   `xml:"firstName,attr"`
+	LastName  string   `xml:"lastName,attr"`
+}
+
+func IndexHandler(c *gin.Context) {
+	c.XML(200, Person{FirstName: "Mohamed",
+		LastName: "Labouardy"})
 }
